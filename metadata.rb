30b3a19baac2fc75b5eb14d8c795d1942486a8b6
Paul Thomas <paul.thomas@livingsocial.com>
2012-08-16 16:45:31 +0000

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Paul Thomas"
+maintainer_email "paul.thomas@livingsocial.com"
+license          "Apache 2.0"
+description      "Installs/Configures ipmi"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
