3175dee291c8d2b1705d7ea0cd9b5120de37c820
Paul Thomas <paul.thomas@livingsocial.com>
2012-10-25 13:41:31 -0700

    update license in line with upstream

--- files/default/ohai-ipmi.rb
+++ files/default/ohai-ipmi.rb
@@ -2,10 +2,19 @@
 # Cookbook Name:: ohai
 # Plugin:: ipmi
 #
-# "THE BEER-WARE LICENSE" (Revision 42):
-# <john@dewey.ws> wrote this file. As long as you retain this notice you
-# can do whatever you want with this stuff. If we meet some day, and you think
-# this stuff is worth it, you can buy me a beer in return John-B Dewey Jr.
+# Copyright 2012, John Dewey
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # Acquired from: https://bitbucket.org/retr0h/ohai.git
 
