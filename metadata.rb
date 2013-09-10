bbf8cbab67d3293e997e6f6cf896dae21a653b3f
Paul Thomas <paul+github@paulthomas.eu>
2013-09-10 09:44:21 -0400

    add new plugin.rb that only installs package and plugin

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "paul+github@paulthomas.eu"
 license          "Apache 2.0"
 description      "Installs/Configures ipmi"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 
 %w{debian ubuntu freebsd}.each do |os|
   supports os
