b9edfdd91179426d841e54a6f86e331bc87a23d8
Paul Thomas <paul+github@paulthomas.eu>
2013-12-16 10:27:07 +0000

    bump version to 0.0.4

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'paul+github@paulthomas.eu'
 license          'Apache 2.0'
 description      'Installs/Configures ipmi'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.3'
+version          '0.0.4'
 
 %w{debian ubuntu freebsd}.each do |os|
   supports os
