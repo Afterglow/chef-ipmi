f15bafe4f35afcf31fe5849e17f56d3c80c74977
Tim Smith <tsmith@llnw.com>
2014-04-30 11:28:18 -0700

    Rubocop fixes and a rubocop config file

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures ipmi'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.8'
 
-%w{debian ubuntu freebsd redhat oracle scientific}.each do |os|
+%w(debian ubuntu freebsd redhat oracle scientific).each do |os|
   supports os
 end
 
